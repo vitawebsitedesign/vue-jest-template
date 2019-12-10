a5dcb637143da1e04e8123d723a92606d4c88107
Michael Nguyen <michael.nguyen@openagent.com.au>
2019-12-10 22:45:39 +1100

    initial commit

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div class="msg">{{ (msg || 'default msg') | uppercase }}</div>
+        <input type="text" v-model="msg">
+        <button id="btnReset" type="button" @click="resetMsg">reset message</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyComponent',
+    data: function() {
+        return {
+            msg: null
+        };
+    },
+    created: function() {
+        this.resetMsg();
+    },
+    methods: {
+        resetMsg: function() {
+            this.msg = null
+        }
+    },
+    filters: {
+        uppercase: function(val) {
+            return val.toUpperCase();
+        }
+    }
+}
+</script>
